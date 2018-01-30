060783cb331862b0570c42297eda76ef324e132c
Joel G Mathew <dz@droidzone.in>
2018-01-30 09:15:23 +0530

    Create LastFivePrescriptionFixer.js

---
+++ LastFivePrescriptionFixer.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Last Five Prescription Fixer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://gateway.esic.in/Hospital/ClinicalRecords/LastFivePrescriptions.aspx*
+// @require       http://code.jquery.com/jquery-3.3.1.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("Prescription fixer loaded");
+
+    // Your code here...
+})();
